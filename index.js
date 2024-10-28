47d72457fa2a77b44b01b2e1d3ffdfe97f9c3d02
joelbarton-io <86934356+joelbarton-io@users.noreply.github.com>
2024-10-28 15:11:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const core = require('@actions/core');
 console.log("Hello from my first GitHub Action!");
+
+const MY_SECRET = core.getInput('my-secret');
+const YOUR_SECRET = core.getInput('your-secret');
+const RUNNER_OS = core.getInput('runner-os');
+
+console.log({MY_SECRET, YOUR_SECRET, RUNNER_OS});
+setTimeout(() => console.log('index.js file finished running...'), 5000);
