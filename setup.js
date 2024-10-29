9d36e12dcef9f8e6bdfe753ca6b015cc39a3ade7
Joel Barton <joelbarton406@gmail.com>
2024-10-29 15:55:42 -0600

    fix: remove setup.js file and update action.yaml Co-authored-by: Shane Ziegler <shaneziegler@users.noreply.github.com>

--- setup.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-    console.log('Installing dependencies...');
-    execSync('npm install', { stdio: 'inherit' });
-} catch (error) {
-    console.error('Failed to install dependencies:', error);
-    process.exit(1);
-}
